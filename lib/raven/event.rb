causes/raven-ruby

commit 9cad1abad4fa00d8947cc7b7928470f11beccf9e

Let Raven::Event instances override a stacktrace interface
It should be possible to override the interface that
Raven::Event.capture_exception uses to handle backtraces. Moving this
constant into a function allows users to override the default.

Change-Id: Ib48f359c3a83a5a58b2d620e78ea346f7600e806
Reviewed-on: https://gerrit.causes.com/9103
Reviewed-by: Aiden Scandella <aiden@causes.com>
Tested-by: Noah Silas <noah@causes.com>

lib/raven/event.rb
@@ -102,7 +102,7 @@ module Raven
         evt.level = :error
         evt.parse_exception(exc)
         if (exc.backtrace)
-          int = evt.interface :stack_trace
+          int = evt.interface evt.stack_trace_interface
           int.frames = exc.backtrace.reverse.map do |trace_line|
             int.frame {|frame| evt.parse_backtrace_line(trace_line, frame) }
           end
@@ -164,6 +164,10 @@ module Raven
     def strip_load_path_from(path)
       prefix = $:.select {|s| path.starts_with?(s)}.sort_by {|s| s.length}.last
       prefix ? path[prefix.chomp(File::SEPARATOR).length+1..-1] : path
+    end
+
+    def stack_trace_interface
+      :stack_trace
     end
   end
 end

commit 9090bcbd9607e5b96ce054657146e144bb534033

Make stack_trace_interface public
A bug introduced in 9cad1abad. I really need to write tests for the
backtrace stuff and squash all this before I submit the pull request for
this refactor.

Change-Id: I968194fa9bf9df50bdef9ddc3fe4316e8ff726a2
Reviewed-on: https://gerrit.causes.com/9164
Reviewed-by: Joe Lencioni <joe.lencioni@causes.com>
Tested-by: Noah Silas <noah@causes.com>

lib/raven/event.rb
@@ -149,6 +149,10 @@ module Raven
       alias :captionException :capture_exception
     end
 
+    def stack_trace_interface
+      :stack_trace
+    end
+
     private
 
     # Because linecache can go to hell
@@ -165,10 +169,6 @@ module Raven
     def strip_load_path_from(path)
       prefix = $:.select {|s| path.start_with?(s)}.sort_by {|s| s.length}.last
       prefix ? path[prefix.chomp(File::SEPARATOR).length+1..-1] : path
-    end
-
-    def stack_trace_interface
-      :stack_trace
     end
   end
 end